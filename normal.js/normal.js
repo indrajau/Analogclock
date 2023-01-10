dbaa88e8b9b73e9d01542235c1511f0af1199b6e
indrajau <119101363+indrajau@users.noreply.github.com>
2023-01-10 17:37:23 +0530

    curring

--- normal.js/normal.js
+++ normal.js/normal.js
@@ -1,21 +1,18 @@
-function x(){
+/*var obj={num:2}
+var funtionName=function(arg1,arg2,arg3){
+    return this.num+arg1+arg2+arg3;
+};
+console.log(funtionName.call(obj,arg1,arg2,arg3));
+console.log(funtionName.apply(obj[arg1,arg2,arg3]));
+var bound=funtionName.bind(obj);
+console.log(bound(arg1,arg2,arg3)); */
 
-    let a = 10;
-    
-    function y(){
-    
-    console.log(a);
-    
-    }
-    
-    a= 50;
-    
-    return y;
-    
-    }
-    
-    
-    
-    const z = x()
-    
-    console.log(z());
+var obj={num : 2};
+var addToThis = function(a,b,c){
+    return this.num+a+b+c;
+}
+console.log(addToThis.call(obj,3));
+console.log(addToThis.call(obj,1,2,3));
+console.log(addToThis.apply(obj,[1,2,3]));
+var bound=addToThis.bind(obj);
+console.log(bound(1,2,3));
